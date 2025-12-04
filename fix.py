ea1cbff3106328b9baca61d437428fa3049b4eaf
Eli Goldner <etgld@posteo.us>
2025-12-04 18:03:27 -0500

    More complicated breakdown

--- fix.py
+++ fix.py
@@ -1,5 +1,8 @@
 import os
+import string
 import argparse
+from functools import lru_cache
+import unicodedata
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument(
@@ -11,6 +14,16 @@ parser.add_argument(
     type=str,
 )
 
+@lru_cache
+def relevant_unicode_category(category: str) -> bool:
+    return category !="So" and not category.startswith("C")
+
+@lru_cache
+def relevant_character(char: str) -> bool:
+    if char in string.printable:
+        return True
+    category = unicodedata.category(char)
+    return relevant_unicode_category(category)
 
 def process(source_dir: str, target_dir: str) -> None:
     for fn in os.listdir(source_dir):
@@ -18,7 +31,7 @@ def process(source_dir: str, target_dir: str) -> None:
             open(os.path.join(source_dir, fn), mode="r") as source_f,
             open(os.path.join(target_dir, fn), mode="w") as target_f,
         ):
-            target_f.write("".join(filter(str.isprintable, source_f.read())))
+            target_f.write("".join(char for char in source_f.read() if relevant_character(char)))
 
 
 def main() -> None:
